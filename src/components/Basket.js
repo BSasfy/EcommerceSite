7a12ccf04d8056158df7f6634ea902ff2caa5f7d
BSasfy <barbara.sasfy@gmail.com>
2020-04-13 20:59:40 +0100

    Sorting and Cart added. Cart needs finetuned.

---
+++ src/components/Basket.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class Basket extends Component {
+    render() {
+        const {cartItems} = this.props;
+        return (
+            <div className="alert alert-info">
+                {cartItems.length===0? "Basket is empty": <div> You have {cartItems.length} products in the basket.</div>}
+                {cartItems.length>0 &&
+                    <div>
+                        <ul>
+                            {cartItems.map(item =>
+                                <li>
+                                    <b>{item.title}</b>
+                                    X {item.count}
+                                    <button className="btn btn-danger"
+                                    onClick={(e) => this.handleRemoveFromCart(e, item)}
+                                    >X</button>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                }
+            </div>
+        )
+    }
+}
